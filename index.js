6f0e4402c73b8fb24dc7929e405d0eacade1878b
izagfr <izadora.g.rosa@aluno.senai.br>
2025-02-25 13:39:40 -0300

    alter5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Ola, Git!");
 console.log("primeiro Branch");
-console.log ("segunda alteração no Branch modulo1   2");
+console.log ("segunda alteração no Branch modulo1");
+console.log("merge");
